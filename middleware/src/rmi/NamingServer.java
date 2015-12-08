bf31277e51cf7844b561eca125d50a1d529a1e54
Camila <camilaascendina@gmail.com>
2015-12-08 12:01:01 -0200

    Adição de novos módulos do servidor de nomes

--- middleware/src/rmi/NamingServer.java
+++ middleware/src/rmi/NamingServer.java
@@ -1,16 +1,29 @@
 package rmi;
 
-import java.rmi.RemoteException;
-import java.rmi.registry.LocateRegistry;
-import java.rmi.registry.Registry;
+import java.io.IOException;
+
+import commonService.NamingInvoker;
+
+//import java.rmi.RemoteException;
+//import java.rmi.registry.LocateRegistry;
+//import java.rmi.registry.Registry;
 
 public class NamingServer {
 	
-	public static void main(String[] args) throws RemoteException, InterruptedException {
-        Registry registry = LocateRegistry.createRegistry(2001);
+	public static void main(String[] args) throws IOException, Throwable {
+        //Registry registry = LocateRegistry.createRegistry(2001);
+		
+		//Chamando os modulos que formam o servidor de nomes
+		NamingInvoker invoker = new NamingInvoker();
+		
+		System.out.println("Servidor de Nomes Pronto para Uso!");
+		
          
-        System.out.println("Registry ready...");
-         
+		//Registrando o numero da porta ao qual o servidor pertence
+		//Aqui a porta utilizada é a 2001
+		invoker.invoke(2001);
+		
+		
         while(true){Thread.sleep(1000);}
 
 	}
