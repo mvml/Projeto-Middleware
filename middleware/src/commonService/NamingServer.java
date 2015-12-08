3edfe32a9ad161a3b4555ac93dd71dc73c260de5
Camila <camilaascendina@gmail.com>
2015-12-08 12:04:12 -0200

    Novos módulos que constituem o servidor de nomes

---
+++ middleware/src/commonService/NamingServer.java
@@ -0,0 +1,14 @@
+package commonService;
+
+import java.io.IOException;
+
+public class NamingServer {
+	
+	public static void main(String[] args) throws IOException, Throwable {
+		NamingInvoker invoker = new NamingInvoker();
+		
+		System.out.println("Naming service ready...");
+		invoker.invoke(1313);
+	}
+
+}
